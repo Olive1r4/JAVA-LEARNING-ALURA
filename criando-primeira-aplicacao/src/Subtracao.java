b3a3829480ea05d30ee2df22dc4cf160ed7c46e1
Fernando Oliveira <fernandooliveira@live.com>
2025-06-05 17:22:37 -0300

    Alura 01

---
+++ criando-primeira-aplicacao/src/Subtracao.java
@@ -0,0 +1,9 @@
+public class Subtracao {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int resultado = a - b;
+
+        System.out.println("A subtração de " + a + " e " + b + " é: " + resultado);
+    }
+}
